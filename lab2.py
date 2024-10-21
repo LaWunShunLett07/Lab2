2eeb2951a4aa0148c99d1af9c654b9223b5be20e
ShunPractice <lawunshunlett@gmail.com>
2024-10-21 09:34:43 +0800

    Lab2 made changes

--- lab2.py
+++ lab2.py
@@ -1,1 +1,12 @@
-print("ET0725 (DevOps for AIoT) - Lab2 - Introduction to Python")
+def calculate_bmi(height,weight):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+    bmi=(weight/(height*height))
+    print("bmi is "+str(bmi))
+    if bmi<18.5:
+        print("It is under weight")
+    elif 18.5<=bmi<=25.0:
+        print("It is normal weight") 
+    elif bmi>25.0:
+        print("It is ove weight")
+calculate_bmi(weight=57,height=1.73)

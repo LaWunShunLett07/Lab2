010eccff018ec2c84b429f8fb29ce6e0006af7a3
ShunPractice <lawunshunlett@gmail.com>
2024-10-21 09:46:35 +0800

    Return type

--- lab2.py
+++ lab2.py
@@ -2,11 +2,6 @@ def calculate_bmi(height,weight):
     print("Height="+str(height))
     print("Weight="+str(weight))
     bmi=(weight/(height*height))
-    print("bmi is "+str(bmi))
-    if bmi<18.5:
-        print("It is under weight")
-    elif 18.5<=bmi<=25.0:
-        print("It is normal weight") 
-    elif bmi>25.0:
-        print("It is ove weight")
-calculate_bmi(weight=57,height=1.73)
+    return bmi
+bmiOutput=calculate_bmi(weight=57,height=1.73)
+print("bmiOutput "+str(bmiOutput))

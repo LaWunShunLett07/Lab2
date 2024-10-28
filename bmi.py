79bc97be8d1512ea5df4bc2233c14959e6f759ca
LaWunShunLett07 <SHUNSHUN.23@ichat.sp.edu.sg>
2024-10-28 10:08:38 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height,weight):
     print("bmi is "+str(bmi))
     if bmi<18.5:
         print("It is under weight")
+        return -1
     elif 18.5<=bmi<=25.0:
-        print("It is normal weight") 
+        print("It is normal weight")
+        return 0
     elif bmi>25.0:
         print("It is ove weight")
+        return 1
 calculate_bmi(weight=57,height=1.73)
